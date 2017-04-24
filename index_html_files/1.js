16404399e2a8ae2c330df8667e109b5805e0abce
ledau106 <ledau.106@gmail.com>
2017-04-24 19:55:48 +0700

    lntt website

---
+++ index_html_files/1.js
@@ -0,0 +1,21 @@
+window.local_nivoslider_22_data = [
+{
+ autoscroll: "5",
+ effect: "1"
+},
+{
+ image: "index_html_files/1580.jpg"
+},
+{
+ image: "index_html_files/1581.jpg"
+},
+{
+ image: "index_html_files/1582.jpg"
+},
+{
+ image: "index_html_files/1583.jpg"
+},
+{
+ image: "index_html_files/1584.jpg"
+}
+];
